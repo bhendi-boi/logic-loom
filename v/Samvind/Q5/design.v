63c4e309e8974ef493f7337181207e0484ca1373
Samrat Chhabra <113725445+Samrat-03@users.noreply.github.com>
2024-03-16 12:45:54 +0530

    Update design.v

--- v/Samvind/Q5/design.v
+++ v/Samvind/Q5/design.v
@@ -1,92 +1,30 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.03.2024 10:43:52
-// Design Name: 
-// Module Name: logic_loom
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module logic_loom(
-    input n,
-    input clk,reset,
-    output reg out
+    input [7:0] n,
+    input clk,
+    output reg [7:0] fib0,
+    output reg [7:0] fib1,
+    output reg [7:0] fib2,
+    output reg [7:0] count
     );
-    
-    parameter A=0, B=1, C=2, D=3, E=4, F=5;
-    
-    reg [2:0] state, next_state;
-    initial state=A;
-    
-    
-    always@(*)begin
-        if (state==A)begin
-            if (n)
-                next_state<=B;
-            else
-                next_state<=A;
-            out<=0;
-        end
-        else if (state==B)begin
-            if(n)
-                next_state<=B;
-            else
-                next_state<=C;
-            out<=0;
-        end
-        else if (state==C)begin
-            if(n)
-                next_state<=D;
-            else
-                next_state<=A;
-            out<=0;
-        end
-        else if (state==D)begin
-            if(n)
-                next_state<=E;
-            else
-                next_state<=C;
-            out<=0;
-        end
-        else if (state==E)begin
-            if(n)
-                next_state<=B;
-            else
-                next_state<=F;
-            out<=0;
-        end
-        else begin
-            if (n) begin
-                next_state<=B;
-                out <= 1;
-            end
-            else begin
-                next_state<=A;
-                out <= 0;
-            end
+
+    initial begin
+        fib0 = 0;
+        fib1 = 1;
+        count = 0;
+       fib2=1;
+    end
+
+    always @(posedge clk) begin
+        if(count < n) begin
+            fib2 = fib0 + fib1;
+            fib0 <= fib1;
+            fib1 <= fib2;
+            count <= count + 1;
+        end else begin
+            fib0 <= 0;
+            fib1 <= 1;
+            fib2<=1;
+            count <= 0;
         end
     end
-    
-    always@(posedge clk) begin
-        if (reset)
-            state <= A;
-        else
-            state <= next_state;
-    end
-    
-    
-    
 endmodule
